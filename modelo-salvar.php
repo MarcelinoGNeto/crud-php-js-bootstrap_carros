75899025c570d9423ac1fb108b6d6a9e2b9cdf2d
MarcelinoGNeto <marcelino.gneto@gmail.com>
2024-01-27 18:01:09 -0300

    crud modelo

--- modelo-salvar.php
+++ modelo-salvar.php
@@ -26,11 +26,37 @@
         break;
 
         case 'editar':
-            # code...
+            $sql = "UPDATE modelo SET
+                    marca_id_marca=".$_POST['marca_id_marca'].",
+                    nome_modelo='".$_POST['nome_modelo']."',
+                    cor_modelo='".$_POST['cor_modelo']."',
+                    ano_modelo='".$_POST['ano_modelo']."',
+                    placa_modelo='".$_POST['placa_modelo']."'
+                WHERE
+                    id_modelo=".$_POST['id_modelo'];
+                    $res = $conn-> query($sql);
+
+            if($res==true){
+                print"<script>alert('Editou com sucesso!');</script>";
+                print"<script>location.href='?page=modelo-listar';</script>";
+            }else{
+                print"<script>alert('Não foi possível editar!');</script>";
+                print"<script>location.href='?page=modelo-listar';</script>";
+            }
         break;
 
         case 'excluir':
-            # code...
+            $sql = "DELETE FROM modelo WHERE id_modelo=".$_REQUEST['id_modelo'];
+
+            $res = $conn-> query($sql);
+
+        if($res==true){
+            print"<script>alert('Excluiu com sucesso!');</script>";
+            print"<script>location.href='?page=modelo-listar';</script>";
+        }else{
+            print"<script>alert('Não foi possível excluir!');</script>";
+            print"<script>location.href='?page=modelo-listar';</script>";
+        }
         break;
     }
 ?>
